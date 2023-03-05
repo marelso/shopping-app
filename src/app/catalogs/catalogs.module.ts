54b035c2bf38fe7e2dcb9e478b6507b8620f5dd5
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 16:01:18 -0300

    Include new dependencies on catalogs module.

--- src/app/catalogs/catalogs.module.ts
+++ src/app/catalogs/catalogs.module.ts
@@ -3,15 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { CatalogsRoutingModule } from './catalogs-routing.module';
 import { CatalogsComponent } from './catalogs.component';
+import { CreateCatalogComponent } from './create-catalog/create-catalog.component';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    CatalogsComponent
+    CatalogsComponent,
+    CreateCatalogComponent
   ],
   imports: [
     CommonModule,
-    CatalogsRoutingModule
+    CatalogsRoutingModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class CatalogsModule { }
