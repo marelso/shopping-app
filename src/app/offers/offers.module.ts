24e1a50710aad15a832af9cf5616d18401e1d73f
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 20:40:41 -0300

    include new imports to offer module

--- src/app/offers/offers.module.ts
+++ src/app/offers/offers.module.ts
@@ -6,12 +6,18 @@ import { OffersRoutingModule } from './offers-routing.module';
 import { OffersComponent } from './offers.component';
 import { CreateOfferComponent } from './create-offer/create-offer.component';
 
-import { MatCardModule } from '@angular/material/card';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
@@ -23,12 +29,18 @@ import { MatIconModule } from '@angular/material/icon';
     FormsModule,
     ReactiveFormsModule,
     OffersRoutingModule,
-    MatCardModule,
     MatStepperModule,
     MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
     MatAutocompleteModule,
+    MatCardModule,
     MatChipsModule,
-    MatIconModule
+    MatIconModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatExpansionModule
   ]
 })
 export class OffersModule { }
