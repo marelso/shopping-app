d20460745bd05656822d771c5b6e31ac5dc30379
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 16:01:45 -0300

    Create new component for creating catalog process

---
+++ src/app/catalogs/create-catalog/create-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-catalog',
+  templateUrl: './create-catalog.component.html',
+  styleUrls: ['./create-catalog.component.scss']
+})
+export class CreateCatalogComponent {
+
+}
