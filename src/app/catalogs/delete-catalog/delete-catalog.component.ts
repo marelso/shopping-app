f9d31be20411ce520ccf32de16b983361ca014d2
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 17:12:55 -0300

    End of development of delete catalog feature.

--- src/app/catalogs/delete-catalog/delete-catalog.component.ts
+++ src/app/catalogs/delete-catalog/delete-catalog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Catalog } from './../../models/Catalog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-catalog',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-catalog.component.scss']
 })
 export class DeleteCatalogComponent {
+  verifyName: string = '';
+  checked: boolean = false;
 
+  constructor(
+    public dialogRef: MatDialogRef<DeleteCatalogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Catalog
+  ) {}
+
+  verifyDelete() {
+    if(this.verifyName === this.data.name && this.checked) {
+      this.dialogRef.close(true);
+    }
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
 }
