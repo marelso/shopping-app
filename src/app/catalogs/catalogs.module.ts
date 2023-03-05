2502d6c5501a7ebeb9c367717cdc6b24eab2c417
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 16:17:59 -0300

    Include new dependencies

--- src/app/catalogs/catalogs.module.ts
+++ src/app/catalogs/catalogs.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { CatalogsRoutingModule } from './catalogs-routing.module';
 import { CatalogsComponent } from './catalogs.component';
+import { ListCatalogComponent } from './list-catalog/list-catalog.component';
 import { CreateCatalogComponent } from './create-catalog/create-catalog.component';
 
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DeleteCatalogComponent } from './delete-catalog/delete-catalog.component';
+import { UpdateCatalogComponent } from './update-catalog/update-catalog.component';
 
 @NgModule({
   declarations: [
     CatalogsComponent,
-    CreateCatalogComponent
+    CreateCatalogComponent,
+    ListCatalogComponent,
+    DeleteCatalogComponent,
+    UpdateCatalogComponent
   ],
   imports: [
     CommonModule,
@@ -25,6 +34,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatAutocompleteModule,
+    MatCardModule,
+    MatDialogModule
   ]
 })
 export class CatalogsModule { }
