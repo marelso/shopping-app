fd5b13dd61d73990b9799694fa78c32e31d6373a
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 20:15:06 -0300

    Including crud functions to offer service.

--- src/app/services/offer.service.ts
+++ src/app/services/offer.service.ts
@@ -1,10 +1,50 @@
+import { HttpClient } from '@angular/common/http';
+import { Offer } from './../models/Offer';
+import { switchMap, take, Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OfferService {
+  private readonly url = 'http://localhost:8080/offers';
 
-constructor() { }
+  private offerCreatedSubject = new Subject<void>();
 
+  constructor(private http: HttpClient) { }
+
+  findAll() {
+    return this.http.get<Offer[]>(this.url);
+  }
+
+  delete(offerId: number) {
+    return this.http.delete(`${this.url}/${offerId}`)
+      .pipe(
+        take(1),
+        switchMap(() => this.findAll())
+      );
+  }
+
+  create(offer: Offer) {
+    return this.http.post(this.url, offer)
+      .pipe(
+        take(1),
+        switchMap(() => {
+          this.offerCreatedSubject.next();
+          return this.findAll();
+        })
+      ).subscribe();
+  }
+
+  update(offer: Offer) {
+    return this.http.put(`${this.url}/${offer.id}`, offer)
+      .pipe(
+        take(1),
+        switchMap(() => this.findAll())
+      );
+  }
+
+  get offerCreated$() {
+    return this.offerCreatedSubject.asObservable();
+  }
 }
