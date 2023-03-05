e622677d56013b32b83f9cf8d8d425917542de3e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 20:30:39 -0300

    Include imports to offer module

--- src/app/offers/offers.module.ts
+++ src/app/offers/offers.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { OffersRoutingModule } from './offers-routing.module';
 import { OffersComponent } from './offers.component';
 import { CreateOfferComponent } from './create-offer/create-offer.component';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +20,15 @@ import { CreateOfferComponent } from './create-offer/create-offer.component';
   ],
   imports: [
     CommonModule,
-    OffersRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    OffersRoutingModule,
+    MatCardModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatChipsModule,
+    MatIconModule
   ]
 })
 export class OffersModule { }
