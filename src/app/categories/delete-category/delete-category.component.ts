8f66589cb1b72692ecd15ee4efac1b73260dbf03
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 19:45:48 -0300

    End of development of delete category feature

--- src/app/categories/delete-category/delete-category.component.ts
+++ src/app/categories/delete-category/delete-category.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Category } from './../../models/Category';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-delete-category',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./delete-category.component.scss']
 })
 export class DeleteCategoryComponent {
+  verifyName: string = '';
+  checked: boolean = false;
 
+  constructor(
+    public dialogRef: MatDialogRef<DeleteCategoryComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Category
+  ) {}
+
+  verifyDelete() {
+    if(this.verifyName === this.data.name && this.checked) {
+      this.dialogRef.close(true);
+    }
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close(false);
+  }
 }
