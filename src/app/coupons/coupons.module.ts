979dfd9c9f3f02e438d75875be7d3f49f9a4e281
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 17:31:14 -0300

    Include user response to empty coupons list.

--- src/app/coupons/coupons.module.ts
+++ src/app/coupons/coupons.module.ts
@@ -12,6 +12,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatCardModule } from '@angular/material/card';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
     MatButtonModule,
     MatPaginatorModule,
     MatCardModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatIconModule
   ]
 })
 export class CouponsModule { }
