21bdcc256a86a9812d27fe4d4ab3c4f35cd2adf7
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 19:47:06 -0300

    End of development of update category feature

--- src/app/categories/update-category/update-category.component.ts
+++ src/app/categories/update-category/update-category.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Category } from './../../models/Category';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-update-category',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-category.component.scss']
 })
 export class UpdateCategoryComponent {
+  updatedCategory: Category = <Category>{ };
 
+  constructor(public dialogRef: MatDialogRef<UpdateCategoryComponent>,
+    @Inject(MAT_DIALOG_DATA) public currentCategory: Category) {
+  }
+
+  update() {
+    this.currentCategory.name = this.updatedCategory.name;
+    this.currentCategory.description = this.updatedCategory.description;
+    this.currentCategory.priority = this.updatedCategory.priority;
+    this.currentCategory.catalogId = this.updatedCategory.catalogId;
+    this.dialogRef.close(this.currentCategory);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
