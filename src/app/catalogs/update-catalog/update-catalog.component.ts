c36cbd9786a957b53756b2762552a04491fc3045
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 17:19:43 -0300

    End of development of update catalog feature.

--- src/app/catalogs/update-catalog/update-catalog.component.ts
+++ src/app/catalogs/update-catalog/update-catalog.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Catalog } from './../../models/Catalog';
+import { Component, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-update-catalog',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./update-catalog.component.scss']
 })
 export class UpdateCatalogComponent {
+  updatedCatalog: Catalog = <Catalog>{ };
 
+  constructor(
+    public dialogRef: MatDialogRef<UpdateCatalogComponent>,
+    @Inject(MAT_DIALOG_DATA) public currentCatalog: Catalog
+  ) {}
+
+  update() {
+    this.currentCatalog.name = this.updatedCatalog.name;
+    this.currentCatalog.description = this.updatedCatalog.description;
+    this.dialogRef.close(this.currentCatalog);
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
