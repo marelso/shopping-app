6d7080dd4a33c80127aafbc4c686218d119dcd8e
Marcelo Klein <marcelo.klein@townsq.com.br>
2023-03-05 18:58:30 -0300

    Include new imports in category module

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoriesComponent } from './categories.component';
 import { CreateCategoryComponent } from './create-category/create-category.component';
 
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @NgModule({
   declarations: [
@@ -13,7 +18,13 @@ import { CreateCategoryComponent } from './create-category/create-category.compo
   ],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatAutocompleteModule
   ]
 })
 export class CategoriesModule { }
